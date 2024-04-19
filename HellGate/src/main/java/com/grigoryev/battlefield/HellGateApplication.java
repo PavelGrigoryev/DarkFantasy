8b51813dadb87076c1020bc7e677c49cd12abbdd
Pavel Grigoriev <pgrigoriev@clevertec.ru>
2024-04-19 17:20:11 +0300

    change module names

--- HellGate/src/main/java/com/grigoryev/battlefield/HellGateApplication.java
+++ HellGate/src/main/java/com/grigoryev/battlefield/HellGateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BattleFieldApplication {
+public class HellGateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BattleFieldApplication.class, args);
+        SpringApplication.run(HellGateApplication.class, args);
     }
 
 }
